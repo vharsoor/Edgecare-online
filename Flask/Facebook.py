369a77e0cc73c9cbc200d602ea524bd7fa7fd8d7
Vishrut Harsoor <harsoorv@gmail.com>
2024-06-20 20:41:18 -0700

    Removing extra facebook backend

--- Flask/Facebook.py
+++ Flask/Facebook.py
@@ -1,85 +1,79 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify
 import requests
 import os
 import urllib.parse
+from flask_cors import CORS
 
 # Allow OAuthlib to use HTTP for local development
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 # Set up Flask app
 app = Flask(__name__)
-
-# Facebook app credentials
-client_id = '281055351768792'
-client_secret = '12aae2a46d55fd214c04f856e5c39fd8'
-public_url = 'http://localhost:5000'
-
-# Redirect URI
-redirect_uri = '{}/auth/facebook/callback'.format(public_url)
-print('Redirect URI: {}'.format(redirect_uri))
+CORS(app) 
 
 # OAuth endpoints
 authorization_base_url = 'https://www.facebook.com/v12.0/dialog/oauth'
 token_url = 'https://graph.facebook.com/v12.0/oauth/access_token'
 
-@app.route("/")
-def index():
-    print("trial 0")
-    auth_params = {
-        'client_id': client_id,
-        'redirect_uri': redirect_uri,
-        'scope': 'user_likes,user_posts',
-        'response_type': 'code'  # Ensure this is included
-    }
-    print("trial1")
-    authorization_url = '{}?{}'.format(authorization_base_url, urllib.parse.urlencode(auth_params))
-    print('Authorization URL: {}'.format(authorization_url))
-    return redirect(authorization_url)
+@app.route('/api/facebook_auth', methods=['POST'])
+def facebook_auth():
+    data = request.json
+    client_id = data.get('client_id')
+    client_secret = data.get('client_secret')
+    public_url = data.get('public_url')
 
-@app.route("/auth/facebook/callback")
+    # Save the credentials securely if needed
+    app.config['CLIENT_ID'] = client_id
+    app.config['CLIENT_SECRET'] = client_secret
+    app.config['PUBLIC_URL'] = public_url
+
+    redirect_uri = f"{public_url}/callback"
+    auth_url = (
+        f"https://www.facebook.com/v12.0/dialog/oauth?client_id={client_id}"
+        f"&redirect_uri={redirect_uri}&scope=user_likes,user_posts"
+    )
+
+    return jsonify({'auth_url': auth_url})
+
+@app.route('/callback', methods=['GET'])
 def callback():
-    print('Received callback with URL: {}'.format(request.url))
+    #if 'CLIENT_ID' not in app.config or 'CLIENT_SECRET' not in app.config or 'PUBLIC_URL' not in app.config:
+        #return "Error: Missing configuration values. Ensure you have set CLIENT_ID, CLIENT_SECRET, and PUBLIC_URL via /api/facebook_auth endpoint.", 400
+    
     code = request.args.get('code')
-    if not code:
-        return "Error: No code provided in callback URL", 400
-    
-    print("Authorization code: {}".format(code))
-    
+    client_id = app.config['CLIENT_ID']
+    client_secret = app.config['CLIENT_SECRET']
+    redirect_uri = f"{app.config['PUBLIC_URL']}/callback"
+
     token_params = {
         'client_id': client_id,
         'redirect_uri': redirect_uri,
         'client_secret': client_secret,
         'code': code
     }
-    
     token_response = requests.get(token_url, params=token_params)
-    print("Token response: {}".format(token_response))
-    
+
     try:
-        token_response.raise_for_status()  # Raise an exception for HTTP errors
-        token = token_response.json().get('access_token')
-        print("Access token: {}".format(token))
-        
-        long_lived_token_response = exchange_access_token(token)
+        token_response.raise_for_status()
+        access_token = token_response.json().get('access_token')
+
+        long_lived_token_response = exchange_access_token(access_token)
         long_lived_token = long_lived_token_response.get('access_token')
-        
+
         user_data = fetch_user_data(long_lived_token)
         if user_data:
-            print('User Info:')
-            print(user_data['userInfo'])
-            return user_data['userInfo']
+            return jsonify(user_data)
         else:
             return 'Error fetching user data', 500
     except requests.exceptions.RequestException as e:
-        print('Error: {}'.format(e))
-        return 'An error occurred: {}'.format(e), 500
+        return f'An error occurred: {e}', 500
 
 def exchange_access_token(access_token):
     url = "https://graph.facebook.com/v12.0/oauth/access_token"
     params = {
         'grant_type': 'fb_exchange_token',
-        'client_id': client_id,
-        'client_secret': client_secret,
+        'client_id': app.config['CLIENT_ID'],
+        'client_secret': app.config['CLIENT_SECRET'],
         'fb_exchange_token': access_token
     }
     response = requests.get(url, params=params)
@@ -95,15 +89,13 @@ def fetch_all_data(url, params):
             data.extend(json_response['data'])
         if 'paging' in json_response and 'next' in json_response['paging']:
             url = json_response['paging']['next']
-            params = None  # After the first page, params should be None because 'next' already includes the access token
+            params = None
         else:
             break
     return data
 
 def fetch_user_data(access_token):
     user_info_url = 'https://graph.facebook.com/me'
-
-    # Define the fields you want to fetch
     user_fields = 'id,name,birthday,friends,posts{source,full_picture,updated_time,status_type}'
 
     params = {
@@ -112,13 +104,11 @@ def fetch_user_data(access_token):
     }
 
     try:
-        # Fetch user info with the specified fields
         user_info_response = requests.get(user_info_url, params=params)
-        user_info_response.raise_for_status()  # Raise an exception for HTTP errors
+        user_info_response.raise_for_status()
 
         user_info = user_info_response.json()
 
-        # Handle pagination for friends and posts
         if 'friends' in user_info:
             friends_data = fetch_all_data('https://graph.facebook.com/me/friends', {
                 'access_token': access_token
@@ -132,13 +122,9 @@ def fetch_user_data(access_token):
             })
             user_info['posts']['data'] = posts_data
 
-        print("user_info : ",user_info)
-        return {
-            'userInfo': user_info,
-        }
-
+        return {'userInfo': user_info}
     except requests.exceptions.RequestException as e:
-        print('Error fetching user data: {}'.format(e))
         return None
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
